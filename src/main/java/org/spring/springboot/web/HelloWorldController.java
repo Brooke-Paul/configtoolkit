302da1c820855357a9fa6f8a4c4209140a6909c8
xuetao <gentle.taoxue@gmail.com>
2019-01-30 23:21:39 +0800

    视图返回

--- src/main/java/org/spring/springboot/web/HelloWorldController.java
+++ src/main/java/org/spring/springboot/web/HelloWorldController.java
@@ -4,9 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Spring Boot HelloWorld案例
- *
- * Created by bysocket on 16/4/26.
+ * 视图返回
  */
 @RestController
 public class HelloWorldController {
