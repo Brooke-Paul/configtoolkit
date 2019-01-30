a0e28d9484ed311055c73e8f676599c1a60183ca
xuetao <gentle.taoxue@gmail.com>
2019-01-30 22:56:12 +0800

    新建springboot demo

---
+++ src/main/java/org/spring/springboot/Application.java
@@ -0,0 +1,14 @@
+package org.spring.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        // 程序启动入口
+        // 启动嵌入式的Tomcat并初始化Spring环境及其各Spring组件
+        SpringApplication.run(Application.class,args);
+    }
+}
